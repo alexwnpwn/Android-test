bac9c775f403b73fe1a7de9f0947d52707585607
alexwnpwn <alex.wnpwn@gmail.com>
2013-05-16 15:13:40 +0200

    firstcommit

---
+++ src/androidgit/testgit/AndroidgitActivity.java
@@ -0,0 +1,13 @@
+package androidgit.testgit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidgitActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
